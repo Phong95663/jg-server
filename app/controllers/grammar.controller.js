6e07557bd6f849946ff77ea64437baa93b7c2354
Phong Vu Van <vuphong95663@gmail.com>
2019-05-06 20:55:39 +0700

    update

--- app/controllers/grammar.controller.js
+++ app/controllers/grammar.controller.js
@@ -10,7 +10,7 @@ const kuroshiro = new Kuroshiro();
 const cabocha = new Cabocha();
 kuroshiro.init(new analyzer());
 
-let parseSring = async (str) => {
+const parseSring = async (str) => {
   return new Promise((resolve, reject) => {
     cabocha.parse(str, (result) => {
       let arr = result.depRels.map(ele => ele[1]);
@@ -19,39 +19,84 @@ let parseSring = async (str) => {
   })
 }
 
-let convertKana = async (str) => {
+const convertKana = async (str) => {
   const res = await kuroshiro.convert(str, { to: "katakana" });
   return res;
 }
+
+const convertVerbToBase = async (str) => {
+  return new Promise((resolve, reject) => {
+    cabocha.parse(str, (result) => {
+      let arr = result.words.map(ele => ele[1] === '動詞' ? ele[7] : ele[0]);
+      resolve(String(arr.join('')));
+    })
+  })
+}
+
+const getUnique = (arr, comp) => {
+
+  const unique = arr
+    .map(e => e[comp])
+
+    // store the keys of the unique objects
+    .map((e, i, final) => final.indexOf(e) === i && i)
+
+    // eliminate the dead keys & store unique objects
+    .filter(e => arr[e]).map(e => arr[e]);
+
+  return unique;
+}
+
+(async () => {
+  // const base = await convertVerbToBase("今週末、どこかに行きませんか。");
+  // const base = await convertVerbToBase("部長はこの時計を買って下さいました");
+  // console.log("=======", base);
+  // console.log(mecab.parseSync('部長はこの時計を買って下さいました').map(w => w[8]));
+  // console.log(typeof (base));
+
+  // console.log(await parseSring("1ヵ月に１回フランス語の作文を書かされました。"));
+  console.log(await parseSring("50歳になって、海外に転勤させられるとは思ってもみなかった"))
+  // console.log(await parseSring("V使役受身"))
+})();
+
 exports.grammar_check = async (req, res) => {
   const arr = new Set();
   const response = [];
-  const inputKana = await convertKana(req.body.data);
-  const inputWordSet = mecab.wakachiSync(req.body.data, (err, result) => {
-    if (err) throw err;
-    // console.log(result);
-  });
-  inputWordSet.push('...');
-  inputWordSet.push('～');
+  console.log("-----------", req.body.data)
+  const base = await convertVerbToBase(req.body.data.replace(/(\r\n|\n|\r)/gm, ""));
+  const inputKana = await convertKana(base);
+  // const inputWordSet = mecab.wakachiSync(req.body.data, (err, result) => {
+  //   if (err) throw err;
+  //   // console.log(result);
+  // });
+  // inputWordSet.push('...');
+  // inputWordSet.push('～');
   // console.log(inputKana)
   let grammars = await grammar.find({}, {_id: 1, title: 1, titleKana:1});
   // console.log(grammars);
-  grammars.map(grammar => {
+  grammars.map(async grammar => {
+    // const titleBase = await convertVerbToBase(grammar.title);
+    // const titleFix = await convertKana(titleBase);
+    // console.log("----------", titleFix);
     let titleFix = grammar.titleKana;
     if (titleFix.includes('～') || grammar.titleKana.includes('...')) {
       titleFix = titleFix.replace('～', '');
       titleFix = titleFix.replace('...', '\\W+');
     }
+
     let pattern = new RegExp(titleFix);
     // console.log(titleFix);
+    // console.log("----------", inputKana);
+    // console.log("==========", titleFix);
     if (inputKana.match(pattern)) {
+      // console.log("========", grammar)
       // console.log('********grammar1', grammar.title)
-      let grammarTitleWordSet = mecab.wakachiSync(grammar.title, function (err, result) {
-        if (err) throw err;
-      });
+      // let grammarTitleWordSet = mecab.wakachiSync(grammar.title, function (err, result) {
+      //   if (err) throw err;
+      // });
       // console.log(inputWordSet);
       // console.log("*****",grammarTitleWordSet);
-      let intersection = _.intersection(grammarTitleWordSet, inputWordSet);
+      // let intersection = _.intersection(grammarTitleWordSet, inputWordSet);
       //So sanh WordSet
       // if (_.isEqual(intersection, grammarTitleWordSet)) {
       //   console.log('******grammar2', grammar.title)
@@ -87,13 +132,14 @@ exports.grammar_check = async (req, res) => {
           count++;
         }
       });
-      if (count == 1) {
+      if (count >= 1) {
         response.push({ id: ele._id, title: ele.title })
       }
     }
   })
-  console.log(response);
-  res.send(response);
+
+  // console.log(getUnique(response, 'title'));
+  res.send(getUnique(response, 'title'));
 };
 exports.get_grammar = async (req, res) => {
   let data = req.body.data;
---
+++ mater.js
@@ -0,0 +1,111 @@
+const grammar = require('../models/grammar.model.js');
+const convert = require('../utils/convertKana');
+const Kuroshiro = require('kuroshiro');
+const analyzer = require('kuroshiro-analyzer-kuromoji');
+const MeCab = require('mecab-async');
+const _ = require('lodash');
+const Cabocha = require('node-cabocha');
+const mecab = new MeCab();
+const kuroshiro = new Kuroshiro();
+const cabocha = new Cabocha();
+kuroshiro.init(new analyzer());
+
+
+let parseSring = async (str) => {
+  return new Promise((resolve, reject) => {
+    cabocha.parse(str, (result) => {
+      let arr = result.depRels.map(ele => ele[1]);
+      resolve(arr);
+    })
+  })
+}
+
+let convertKana = async (str) => {
+  const res = await kuroshiro.convert(str, { to: "katakana" });
+  return res;
+}
+
+let convertVerbToBase = async (str) => {
+  return new Promise((resolve, reject) => {
+    cabocha.parse(str, (result) => {
+      let arr = result.words.map(ele => ele[1] === '動詞' ? ele[7] : ele[0]);
+      resolve(String(arr.join('')));
+    })
+  })
+}
+
+//Example: てくださる => the cua dong tu
+
+(async () => {
+  const base = await convertVerbToBase("今週末、どこかに行きませんか。");
+
+  console.log(mecab.parseSync('今週末、どこかに行きませんか。').map(w => w[8]));
+  console.log(typeof (base));
+
+  // console.log(await parseSring("1ヵ月に１回フランス語の作文を書かされました。"));
+  // console.log(await parseSring("50歳になって、海外に転勤させられるとは思ってもみなかった"))
+  // console.log(await parseSring("V使役受身"))
+})();
+exports.grammar_check = async (req, res) => {
+  const arr = new Set();
+  const response = [];
+  const base = await convertVerbToBase(req.body.data)
+  const inputKana = await convertKana(base);
+  // console.log(inputKana)
+  const inputWordSet = mecab.wakachiSync(base, (err, result) => {
+    if (err) throw err;
+    // console.log(result);
+  });
+  console.log(inputWordSet);
+  const inputWordSetToString = inputWordSet.join(' ');
+  const inputWordSetToStringToKana = await convertKana(inputWordSetToString);
+  const inputWordSetKana = inputWordSetToStringToKana.split(' ');
+  // const inputWordSetKana = inputWordSet.map(word, async (word)=> { await convertKana(word) });
+  console.log(inputWordSetKana);
+  inputWordSetKana.push('...');
+  inputWordSetKana.push('～');
+  // console.log(inputKana)
+  let grammars = await grammar.find();
+  // console.log(grammars);
+  grammars.map(grammar => {
+    let titleFix = grammar.titleKana;
+    if (titleFix.includes('～') || grammar.titleKana.includes('...')) {
+      titleFix = titleFix.replace('～', '');
+      titleFix = titleFix.replace('...', '\\W+');
+    }
+    let pattern = new RegExp(titleFix);
+    // console.log(titleFix);
+    if (inputKana.match(pattern)) {
+      // let grammarTitleWordSetKana = mecab.wakachiSync(grammar.title, function (err, result) {
+      //   if (err) throw err;
+      // });
+      let grammarTitleWordSetKana = mecab.parseSync(grammar.title).map(word => word[8]);
+      // console.log(inputWordSet);
+      console.log("*****", grammarTitleWordSetKana);
+      let intersection = _.intersection(grammarTitleWordSetKana, inputWordSetKana);
+      //So sanh WordSet
+      if (_.isEqual(intersection, grammarTitleWordSetKana)) {
+        arr.add(grammar.title);
+      }
+    }
+  })
+  Array.from(arr).map(ele => {
+    let eleText = ele.replace('...', '').replace('～', '');
+    let count = 0;
+    Array.from(arr).forEach(ele => {
+      if (ele.includes(eleText)) {
+        count++;
+      }
+    });
+    if (count == 1) {
+      response.push({ title: ele })
+    }
+  });
+  // console.log(response);
+  res.send(response);
+};
+exports.get_grammar = async (req, res) => {
+  let queryString = req.query;
+  let grammars = await grammar.find({ title: queryString.title });
+  res.send(grammars);
+};
